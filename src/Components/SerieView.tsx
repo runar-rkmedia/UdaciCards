d07b2ae0bdf13c15f442bbbdef8044116e6dbdf3
Runar Kristoffersen <public@rkmedia.no>
2017-12-18 09:03:30 +0100

    Basic answerable questions

--- src/Components/SerieView.tsx
+++ src/Components/SerieView.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Card, DeckSwiper, Left, Body, CardItem, Icon } from 'native-base'
+import { View, Text, Card, DeckSwiper, Left, Body, CardItem, Button } from 'native-base'
 import { Serie } from '../store'
 import { connect } from 'react-redux'
 import { StoreState, Card as CardI } from '../store'
@@ -8,10 +8,30 @@ interface Props {
   serie: Serie
 }
 
-export class SerieViewC extends React.Component<Props & IConnectProps> {
+interface State {
+  answers: {
+    [s: string]: any
+  }
+}
+
+const initialState: State = {
+  answers: {}
+}
+
+export class SerieViewC extends React.Component<Props & IConnectProps, State> {
+  state = initialState
+  answer = (cardID: string, value: any) => {
+    this.setState((state): State => ({
+      ...state,
+      answers: {
+        ...state.answers,
+        [cardID]: value
+      }
+    }))
+  }
   render() {
-    console.log('series', this.props)
     const { serie, cards } = this.props
+    const { answers } = this.state
     if (!serie) {
       return (
         <View>
@@ -22,26 +42,38 @@ export class SerieViewC extends React.Component<Props & IConnectProps> {
     return (
       <View>
         <DeckSwiper
-          dataSource={Object.keys(cards).map(key => cards[key])}
+          dataSource={cards}
           renderItem={(card: CardI) => {
-            const { question, numeral, options, answer } = card
-            console.log(card )
+            const { question, numeral, options } = card
+            const index = cards.indexOf(card)
+            const alreadyAnswered = answers[card.id]
             return (
               <Card style={{ elevation: 3 }}>
                 <CardItem>
                   <Left>
                     <Body>
-                      <Text>{question}</Text>
+                      <Text>{`${index + 1}/${cards.length}: ${question}`}</Text>
                       <Text note={true}>{numeral ? 'How much...' : options ? 'What...' : null}</Text>
                     </Body>
                   </Left>
                 </CardItem>
-                <CardItem cardBody={true}>
-                  <Text>Show buttons here</Text>
-                </CardItem>
                 <CardItem>
-                  <Icon name="heart" style={{ color: '#ED4A6A' }} />
-                  <Text>({answer})</Text>
+                  {options && options.map((option, i) => {
+                    const answeredThisOption = alreadyAnswered === option.displayText
+                    console.log(alreadyAnswered, answeredThisOption, answers)
+                    return (
+                    <Button
+                      light={!answeredThisOption}
+                      primary={option.correct}
+                      success={answeredThisOption && option.correct}
+                      warning={answeredThisOption && !option.correct}
+                      info={!alreadyAnswered}
+                      key={i}
+                      onPress={() => this.answer(card.id, option.displayText)}
+                    >
+                      <Text>{option.displayText}</Text>
+                    </Button>
+                  )})}
                 </CardItem>
               </Card>
             )
@@ -57,15 +89,9 @@ export class SerieViewC extends React.Component<Props & IConnectProps> {
 const connectCreator = connect(
   ({ cards }: StoreState, { serie }: Props) => {
     return {
-      cards: serie && Object.keys(cards).reduce(
-        (x, key) => {
-          const card = cards[key]
-          if (card.seriesId === serie.id) {
-            return { ...x, card }
-          }
-          return x
-        },
-        {})
+      cards: serie && Object.keys(cards)
+        .map(key => cards[key])
+        .filter(card => card.seriesId === serie.id)
     }
   },
 )
